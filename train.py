b98fd04b0a747a12dbc353c027a9e541e398ffd7
Jimmie Munyi <jimmiemunyi@gmail.com>
2023-07-12 12:41:04 +0300

    baseline -0.42337

---
+++ train.py
@@ -0,0 +1,110 @@
+from pathlib import Path
+
+from wasabi import Printer
+import hydra
+from omegaconf import DictConfig, OmegaConf
+
+import pandas as pd
+
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+
+from composer.models import HuggingFaceModel
+from composer.metrics import CrossEntropy
+from composer import Trainer
+from composer.loggers import WandBLogger
+
+from torchmetrics import F1Score
+
+from utils import (
+    create_label_dicts,
+    prepare_data,
+    prepare_optimizer_and_scheduler,
+    predict,
+)
+
+logger = Printer()
+
+
+@hydra.main(version_base=None, config_path="config", config_name="train")
+def train(cfg: DictConfig):
+    logger.divider("Setting up")
+    print(f"Configurations:\n{OmegaConf.to_yaml(cfg)}")
+
+    logger.info("Loading the dataset")
+    path = Path(cfg.data.path)
+
+    train_df = pd.read_csv(path / "Train.csv", index_col="Id")
+    test_df = pd.read_csv(path / "test.csv", index_col="Id")
+    label2int, int2label = create_label_dicts(train_df)
+    train_df["labels"] = train_df.medical_specialty.map(label2int)
+    num_labels = len(label2int)
+    print(f"Number of labels: {num_labels}")
+
+    logger.info("Tokenizing and splitting the dataset")
+    tokenizer = AutoTokenizer.from_pretrained(cfg.train.checkpoint)
+    train_dl, val_dl = prepare_data(
+        train_df, tokenizer, cfg.data.sep_token, cfg.train.batch_size
+    )
+
+    logger.info("Loading the model, optimizer, and scheduler")
+    f1_score = F1Score(task="multiclass", num_classes=num_labels)
+    ptrn_model = AutoModelForSequenceClassification.from_pretrained(
+        cfg.train.checkpoint,
+        num_labels=num_labels,
+    )
+    composer_model = HuggingFaceModel(
+        model=ptrn_model,
+        tokenizer=tokenizer,
+        metrics=[CrossEntropy(), f1_score],
+        use_logits=True,
+        # allow_embedding_resizing=True
+    )
+
+    # preparing optimizer and scheduler
+    optimizer, scheduler = prepare_optimizer_and_scheduler(
+        composer_model, cfg.train.lr, cfg.train.wd, cfg.train.epochs, train_dl
+    )
+
+    if cfg.run_name:
+        run_name = cfg.run_name
+    else:
+        run_name = cfg.train.checkpoint.split("/")[-1]
+
+    loggers = []
+    if cfg.wandb:
+        loggers.append(WandBLogger(project="medical-specialty-classification"))
+
+    logger.info("Loading the trainer")
+    trainer = Trainer(
+        model=composer_model,
+        train_dataloader=train_dl,
+        eval_dataloader=val_dl,
+        max_duration=f"{cfg.train.epochs}ep",
+        optimizers=optimizer,
+        schedulers=[scheduler],
+        loggers=loggers,
+        run_name=run_name,
+        device="gpu",
+        precision="amp_fp16",
+        step_schedulers_every_batch=True,
+        # seed=17,
+    )
+
+    logger.divider("Training")
+    trainer.fit()
+    logger.good(f"Final Validation Scores: {trainer.state.eval_metric_values}")
+
+    if cfg.create_csv:
+        logger.divider("Inference")
+        test_dl = prepare_data(
+            test_df, tokenizer, cfg.data.sep_token, len(test_df), training=False
+        )
+        preds = predict(trainer, test_dl, int2label)
+        logger.info("Creating submission.csv")
+        submission = pd.DataFrame({"Id": test_df.index, "medical_specialty": preds})
+        submission.to_csv("submission.csv", index=False)
+        logger.good("submission.csv created!")
+
+
+if __name__ == "__main__":
+    train()
---
+++ utils.py
@@ -0,0 +1,209 @@
+"""Some Utility Functions that help iterate faster."""
+from functools import partial
+
+import pandas as pd
+import numpy as np
+
+from torch.utils.data import DataLoader
+from torch.optim import AdamW
+from torch.optim.lr_scheduler import OneCycleLR
+
+import datasets
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from transformers.data.data_collator import default_data_collator
+
+from composer import Trainer
+from composer.models import HuggingFaceModel
+from composer.metrics import CrossEntropy
+from composer.loggers import WandBLogger
+
+from torchmetrics import F1Score
+
+
+def process_df(df: pd.DataFrame, sep_token: str):
+    df["input"] = df.description
+    df.input = df.input.str.lower()
+    return df
+
+
+def create_val_split(df: pd.DataFrame, val_prop: float = 0.2, seed: int = 42):
+    sample_name = df.sample_name.unique()
+    np.random.seed(seed)
+    np.random.shuffle(sample_name)
+    val_sz = int(len(sample_name) * val_prop)
+    val_sample_name = sample_name[:val_sz]
+    is_val = np.isin(df.sample_name, val_sample_name)
+    idxs = np.arange(len(df))
+    val_idxs = idxs[is_val]
+    trn_idxs = idxs[~is_val]
+
+    return trn_idxs, val_idxs
+
+
+def tokenize_func(batch, tokenizer):
+    return tokenizer(
+        batch["input"],
+        padding=True,
+        truncation=True,
+        return_tensors="pt",
+    )
+
+
+def create_label_dicts(df: pd.DataFrame):
+    label2int = {label: i for i, label in enumerate(df.medical_specialty.unique())}
+    int2label = {v: k for k, v in label2int.items()}
+
+    return label2int, int2label
+
+
+def tokenize_and_split(df, tokenize_func, train=True):
+    remove_columns = ["sample_name", "input", "transcription"]
+    dataset = datasets.Dataset.from_pandas(df)
+    tok_dataset = dataset.map(
+        tokenize_func, batched=True, batch_size=None, remove_columns=remove_columns
+    )
+    if train:
+        # remove_columns.append("medical_specialty")
+        trn_idxs, val_idxs = create_val_split(df)
+        tok_dataset = datasets.DatasetDict(
+            {
+                "train": tok_dataset.select(trn_idxs),
+                "test": tok_dataset.select(val_idxs),
+            }
+        )
+
+    return tok_dataset
+
+
+def create_dataloaders(tok_ds, bs, train=True):
+    if train:
+        train_dl = DataLoader(
+            tok_ds["train"],
+            batch_size=bs,
+            shuffle=True,
+            collate_fn=default_data_collator,
+            drop_last=True,
+        )
+        val_dl = DataLoader(
+            tok_ds["test"],
+            batch_size=bs,
+            shuffle=False,
+            collate_fn=default_data_collator,
+            drop_last=True,
+        )
+
+        return train_dl, val_dl
+    else:
+        test_dl = DataLoader(
+            tok_ds,
+            batch_size=bs,
+            shuffle=False,
+            collate_fn=default_data_collator,
+        )
+
+        return test_dl
+
+
+def prepare_data(df, tokenizer, sep_token, bs, training=True):
+    if training:
+        train_df = process_df(df, sep_token)
+        tokenize = partial(tokenize_func, tokenizer=tokenizer)
+        train_tok_ds = tokenize_and_split(train_df, tokenize)
+        train_dl, val_dl = create_dataloaders(train_tok_ds, bs)
+
+        return train_dl, val_dl
+    else:
+        test_df = process_df(df, sep_token)
+        tokenize = partial(tokenize_func, tokenizer=tokenizer)
+        test_tok_ds = tokenize_and_split(test_df, tokenize, train=False)
+        test_dl = create_dataloaders(test_tok_ds, bs, train=False)
+
+        return test_dl
+
+
+def prepare_optimizer_and_scheduler(model, lr, wd, epochs, train_dl):
+    optimizer = AdamW(
+        params=model.parameters(),
+        lr=lr,
+        betas=(0.9, 0.98),
+        eps=1e-6,
+        weight_decay=wd,
+    )
+    scheduler = OneCycleLR(
+        optimizer,
+        max_lr=lr,
+        steps_per_epoch=len(train_dl),
+        epochs=epochs,
+    )
+
+    return optimizer, scheduler
+
+
+def prepare_model(checkpoint, tokenizer, num_labels):
+    f1_score = F1Score(task="multiclass", num_classes=num_labels)
+    model = AutoModelForSequenceClassification.from_pretrained(
+        checkpoint,
+        num_labels=num_labels,
+    )
+    hf_model = HuggingFaceModel(
+        model, tokenizer=tokenizer, metrics=[CrossEntropy(), f1_score], use_logits=True
+    )
+
+    return hf_model
+
+
+def prepare_trainer(
+    composer_model, optimizer, scheduler, train_dl, val_dl, epochs, run_name
+):
+    trainer = Trainer(
+        model=composer_model,
+        train_dataloader=train_dl,
+        eval_dataloader=val_dl,
+        max_duration=f"{epochs}ep",
+        optimizers=optimizer,
+        schedulers=[scheduler],
+        loggers=[WandBLogger(project="medical-specialty-classification")],
+        run_name=run_name,
+        device="gpu",
+        precision="amp_fp16",
+        step_schedulers_every_batch=True,
+        # seed=17,
+    )
+
+    return trainer
+
+
+def fit(
+    train_df,
+    cfg,
+    run_name,
+    num_labels,
+):
+    # preparing data
+    tokenizer = AutoTokenizer.from_pretrained(cfg.train.checkpoint)
+    train_dl, val_dl = prepare_data(
+        train_df, tokenizer, cfg.train.sep_token, cfg.train.batch_size
+    )
+    model = prepare_model(cfg.train.checkpoint, tokenizer, num_labels)
+
+    # preparing optimizer and scheduler
+    optimizer, scheduler = prepare_optimizer_and_scheduler(
+        model, cfg.train.lr, cfg.train.wd, cfg.train.epochs, train_dl
+    )
+
+    # preparing trainer
+    trainer = prepare_trainer(
+        model, optimizer, scheduler, train_dl, val_dl, cfg.train.epochs, run_name
+    )
+
+    # training
+    trainer.fit()
+
+    return trainer
+
+
+def predict(trainer, test_dl, int2label):
+    preds = trainer.predict(test_dl)[0]["logits"].argmax(1).numpy().astype(int)
+    preds = preds.tolist()
+    preds = [int2label[pred] for pred in preds]
+    return preds
